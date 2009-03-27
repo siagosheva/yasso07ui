2320d5bd1e246b8d620d1286319d8cb9f8ec91df
jussi.rasinmaki@simosol.fi <jussi.rasinmaki@simosol.fi@52967dc2-aa7f-11dd-91bf-632da8b6e10b>
2009-03-27 09:24:22 +0000

    tagging the 1.0 release of yasso07ui

---
+++ tags/release-1.0/pyinstaller/unpackMetadata.py
@@ -0,0 +1,51 @@
+import carchive
+import sys
+import os
+import uuid
+import colorsys
+import shelve
+import cgi
+
+#if hasattr(sys, 'frozen'):
+#    __file__ = ''.join((os.path.dirname(sys.executable), 'unpackMetadata.py'))
+
+this = carchive.CArchive(sys.executable)
+archives = os.environ['_MEIPASS2']
+pkgs = ['wx', 'traits', 'traitswx', 'chaco', 'enthought', 'enable', 'traitsgui']
+for pkg in pkgs:
+    mp = this.openEmbedded('%s.pkg' % pkg)
+    targetdir = os.path.join(archives,pkg)
+    os.mkdir(targetdir)
+    print "Extracting %s ..." % pkg,
+    for fnm in mp.contents():
+        try:
+            stuff = mp.extract(fnm)[1]
+            outnm = os.path.join(targetdir, fnm)
+            dirnm = os.path.dirname(outnm)
+            if not os.path.exists(dirnm):
+                os.makedirs(dirnm)
+            open(outnm, 'wb').write(stuff)
+        except Exception, mex:
+            print mex
+    mp = None
+    sys.path.insert(0, targetdir) 
+    if pkg == 'traitsgui':
+        imagepath = os.path.join(targetdir, 'enthought', 'traits', 'ui', 'image', 'library')
+        os.environ['TRAITS_IMAGES'] = imagepath
+        os.putenv('TRAITS_IMAGES', imagepath)
+    print "ok"
+
+import code, keyword
+import wx
+import wx.html
+import wx.lib.scrolledpanel
+from wx import *
+
+from resource_path_override import ResourcePathOverride    
+resource_override = ResourcePathOverride(archives)
+
+#DEBUG        
+#print "Files are at", archives 
+#print "sys.path is:"
+#for i in sys.path: print i
+#raw_input("press enter to continue...")
